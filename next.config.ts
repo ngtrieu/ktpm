f571ec1bbaa69ad96ba71ee5bff942b6047e63e8
datwhite21 <datnguyen2k521@gmail.com>
2025-06-08 23:41:44 +0700

    Add files via upload

---
+++ next.config.ts
@@ -0,0 +1,23 @@
+import type { NextConfig } from "next";
+
+const nextConfig: NextConfig = {
+  /* config options here */
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "image.mux.com",
+      },
+      {
+        protocol: "https",
+        hostname: "utfs.io",
+      },
+      {
+        protocol: "https",
+        hostname: "wr50pkcizm.ufs.sh",
+      },
+    ],
+  },
+};
+
+export default nextConfig;
